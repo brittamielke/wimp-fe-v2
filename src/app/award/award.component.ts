eb52353a75c82700ad323b90ea78be60243d0d65
Britta Mielke <britta.mielke@libertymutual.com>
2018-01-04 14:43:07 -0500

    actor and movie updated, not award yet

---
+++ src/app/award/award.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material';
+
+import { DataService } from '../data.service'
+import { DeleteConfirmComponent } from '../delete-confirm/delete-confirm.component'
+import { fadeInAnimation } from '../animations/fade-in.animation';
+
+
+@Component({
+  selector: 'app-award',
+  templateUrl: './award.component.html',
+  styleUrls: ['./award.component.css'],
+  animations: [fadeInAnimation]
+})
+export class AwardComponent implements OnInit {
+
+  errorMessage: string;
+  successMessage: string;
+  awards: any[];
+  
+  constructor(private dataService: DataService, public dialog: MatDialog) { }
+
+  getAwards() {
+    this.dataService.getRecords("awards")
+      .subscribe(
+      awards => this.awards = awards,
+      error => this.errorMessage = <any>error);
+  }
+
+  deleteAward(id: number) {
+
+    let dialogRef = this.dialog.open(DeleteConfirmComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.dataService.deleteRecord("awards", id)
+          .subscribe(
+            award => {
+            this.successMessage = "Record(s) deleted successfully";
+            this.getAwards();
+          },
+          error => this.errorMessage = <any>error);
+      }
+    });
+  }
+
+  ngOnInit() {
+    this.getAwards();
+  }
+}
